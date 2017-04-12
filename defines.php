c70735cd8938a8afdc1136de0139b64c88c2152d
Zarna0x <justnightmare123@gmail.com>
2017-04-12 08:06:57 +0400

    add travis

--- defines.php
+++ defines.php
@@ -1,12 +1,8 @@
 <?php
 
-  DEFINE('BASE_URL','http://localhost/phproot/shadow/');
-  DEFINE('BASE_PATH',"shadowapp/sh_views");
-  DEFINE('CACHE_DIR', 'shadowapp/sh_cache');
-
 function setDir($dirPath)
 {
-	global $dirStack;
+
 	$dirList = scandir($dirPath);
     
 	for($i = 0; $i < count($dirList); $i++){
