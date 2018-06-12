d3c345aa49477135159c989dccbf77b80f8b8a8e
Zarna0x <justnightmare123@gmail.com>
2018-06-12 21:30:59 +0400

    qwerty

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -43,8 +43,8 @@ class ComposerStaticInite41b8f5ceb259a28858275a08f7fba95
         'phpDocumentor\\Reflection\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpdocumentor/reflection-common/src',
-            1 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
-            2 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            1 => __DIR__ . '/..' . '/phpdocumentor/reflection-docblock/src',
+            2 => __DIR__ . '/..' . '/phpdocumentor/type-resolver/src',
         ),
         'Webmozart\\Assert\\' => 
         array (
