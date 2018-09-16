25b50240a56d46a7bd87ecf01426037395dcf82f
Zarna0x <justnightmare123@gmail.com>
2018-09-16 21:05:55 +0400

    add composer check

--- stack.php
+++ stack.php
@@ -1,5 +1,13 @@
 <?php
 
+$composerFile = __DIR__ . '/vendor/autoload.php';
+
+if (false === file_exists($composerFile)) {
+   echo 'Please run composer install first';
+   exit;
+}
+
+
 $loader = require __DIR__ . '/vendor/autoload.php';
 include_once 'defines.php';
 
