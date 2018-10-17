27454c5420545ac212844c33aef4c3d9b45b4170
System Administrator <root@RealtynaLex.local>
2018-10-17 13:12:37 -0400

    .

--- shadowapp/Sys/Router.php
+++ shadowapp/Sys/Router.php
@@ -1,283 +1,253 @@
 <?php
+
 /*
  * Router 
  * @Author Zarna0x
  * (c) 2016
  */
+
 namespace Shadowapp\Sys;
 
-class Router
-{
-   /*
-	* @var array
-	*/
+class Router {
+    /*
+     * @var array
+     */
+
     protected static $_routes;
-    
-   /*
-	* @var string
-	*/
+
+    /*
+     * @var string
+     */
     protected static $_currentRequstMethod;
-    
-   /*
-	* @var array
-	*/
-    protected static $_requestList = [ 'AJAX', 'GET', 'POST' ];
-    
-   /*
-	* @var string
-	*/
+
+    /*
+     * @var array
+     */
+    protected static $_requestList = ['AJAX', 'GET', 'POST'];
+
+    /*
+     * @var string
+     */
     protected static $defaultApiPrefix = 'api';
-
     protected static $customApiPrefixes = [];
-
     private static $withPrefixCounter = 0;
-   
-
-   /*
-	* @var array
-	*/
-	protected static $apiRoutes = [];
-
-	/*
-	 * Define routes and collect it to $_uri array
-	 * @param type $uri
-	 */
-	public static function define( $uri, $method = null, $request_type = "get" )
-	{  
-	    self::$_routes[strtoupper( $request_type )][trim( $uri, '/' )] = $method;
-        self::$_currentRequstMethod = shcol("REQUEST_METHOD",$_SERVER);
-	}
 
 
-	public static function api ($apiUri, $method = null, $request_type = "get")
-	{
+    /*
+     * @var array
+     */
+    protected static $apiRoutes = [];
 
-	 self::$apiRoutes[strtoupper( $request_type )][trim( $apiUri, '/' )] = [
-           'apiPrefix' => self::getPrefix(),
-           'action'    => $method 
-		];
-		return new static;
-	}
+    /*
+     * Define routes and collect it to $_uri array
+     * @param type $uri
+     */
 
-	public static function withPrefix(string $prefix) 
-	{
-     
-       if ($prefix != self::$defaultApiPrefix && !empty($prefix) && is_string($prefix)) {
-           self::$customApiPrefixes[self::$withPrefixCounter] = $prefix;
+    public static function define($uri, $method = null, $request_type = "get") {
+        self::$_routes[strtoupper($request_type)][trim($uri, '/')] = $method;
+        self::$_currentRequstMethod = shcol("REQUEST_METHOD", $_SERVER);
+    }
+
+    public static function api($apiUri, $method = null, $request_type = "get") {
+
+        self::$apiRoutes[strtoupper($request_type)][trim($apiUri, '/')] = [
+            'apiPrefix' => self::getPrefix(),
+            'action' => $method
+        ];
+        return new static;
+    }
+
+    public static function withPrefix(string $prefix) {
+
+        if ($prefix != self::$defaultApiPrefix && !empty($prefix) && is_string($prefix)) {
+            self::$customApiPrefixes[self::$withPrefixCounter] = $prefix;
         }
-      return new static;
-	}
+        return new static;
+    }
 
-	protected static function getPrefix()
-	{ 
+    protected static function getPrefix() {
 
-	  $currentPrefixCount = self::$withPrefixCounter;
+        $currentPrefixCount = self::$withPrefixCounter;
 
-	  self::$withPrefixCounter++;
+        self::$withPrefixCounter++;
 
-	  return shcol($currentPrefixCount,self::$customApiPrefixes,self::$defaultApiPrefix); 
-	}
+        return shcol($currentPrefixCount, self::$customApiPrefixes, self::$defaultApiPrefix);
+    }
 
-	
+    /*
+     * Run Routes
+     */
 
-	/*
-	 * Run Routes
-	 */
-	public static function run()
-	{
+    public static function run() {
 
-		/*
-		 * Parse Uri
-		 */
-		$uri = isset( $_GET['uri'] ) ? trim( strip_tags( $_GET['uri'] ) ) : '';
-		$uriArr = explode( '/', $uri );
+        /*
+         * Parse Uri
+         */
+        $uri = isset($_GET['uri']) ? trim(strip_tags($_GET['uri'])) : '';
+        $uriArr = explode('/', $uri);
 
 
-		#check if current uri belongs to Route
+        #check if current uri belongs to Route
 
 
-		/*
-		 * Custom route To Load
-		 */
-		$uriCustom = $uriArr[0];
+        /*
+         * Custom route To Load
+         */
+        $uriCustom = $uriArr[0];
 
-		/*
-		 * Method To Load
-		 */
-		$uriMethod = isset( $uriArr[1] ) ? $uriArr[1] : null;
-        $routedUri = trim(shcol(0,explode('/', $uri)));
+        /*
+         * Method To Load
+         */
+        $uriMethod = isset($uriArr[1]) ? $uriArr[1] : null;
+        $routedUri = trim(shcol(0, explode('/', $uri)));
 
         $from = 'web';
 
-        
-        if ( false === array_key_exists($routedUri, self::$_routes[self::$_currentRequstMethod]) ) {
-             if (false === self::apiEndpointExists( $uri )) {
+
+        if (false === array_key_exists($routedUri, self::$_routes[self::$_currentRequstMethod])) {
+            if (false === self::apiEndpointExists($uri)) {
                 echo 'No Route for given uri';
                 exit;
-         	 }
-             
-           $from = 'api';
-         }
+            }
+
+            $from = 'api';
+        }
 
 
-		 self::exec($routedUri,$uri,$from);
-	}
+        self::exec($routedUri, $uri, $from);
+    }
 
-	private static function exec($routedUri,$uri,$appType)
-	{
-           if (!in_array($appType, ['web','api'])) {
-               echo 'Wrong Application type';
-               exit;
-           }
+    private static function exec($routedUri, $uri, $appType) {
+        if (!in_array($appType, ['web', 'api'])) {
+            echo 'Wrong Application type';
+            exit;
+        }
 
-			if ( empty( $routedUri ) && !empty( $uri ) )
-			{
-				
-				$routedUri = md5( rand( 0, 999999999999 ) );
-			}
-           
-            if ( $appType == 'api' ) {
-                
-                $routedUri = implode('/',array_filter(explode('/',$uri),function ($value,$key) {
-                	if ($key > 0) {
-                        return true;
-                	}
-                },ARRAY_FILTER_USE_BOTH));
-                  
+        if (empty($routedUri) && !empty($uri)) {
+
+            $routedUri = md5(rand(0, 999999999999));
+        }
+
+        if ($appType == 'api') {
+
+            $routedUri = implode('/', array_filter(explode('/', $uri), function ($value, $key) {
+                        if ($key > 0) {
+                            return true;
+                        }
+                    }, ARRAY_FILTER_USE_BOTH));
+        }
+
+
+
+        $routedArg = ($appType == 'web') ?
+                shcol($routedUri, self::$_routes[self::$_currentRequstMethod]) :
+                shcol("{$routedUri}.action", self::$apiRoutes[self::$_currentRequstMethod]);
+
+
+        /*
+         * Check Route::Define arguments
+         */
+
+        if (is_array($routedArg)) {
+            if (false === array_key_exists('controller', $routedArg)) {
+
+                echo "You must specify Controller.";
+                exit;
             }
 
 
-          
-			$routedArg = ($appType == 'web') ?
-			    shcol($routedUri,self::$_routes[self::$_currentRequstMethod]):
-                shcol("{$routedUri}.action",self::$apiRoutes[self::$_currentRequstMethod]);
+            $controllerName = "Shadowapp\\Controllers\\" . ucfirst($routedArg['controller']) . "Shadow";
 
 
-					/*
-					 * Check Route::Define arguments
-					 */
+            $methodName = isset($routedArg['method']) ? $routedArg['method'] : null;
 
-					if ( is_array( $routedArg ) )
-					{
-                      if ( false === array_key_exists('controller', $routedArg) ) {
-                            
-                            echo "You must specify Controller.";
-                            exit;
+
+            /*
+             * Check If Class Exists
+             */
+            if (class_exists($controllerName) == false) {
+
+                echo $controllerName . " doesn't exists";
+                return;
+            }
+
+            /*
+             * Check if Method Exists
+             */
+
+            if (isset($routedArg['method'])) {
+
+                if (method_exists($controllerName, $methodName)) {
+
+                    $paramString = trim(substr($uri, strlen($routedUri)), '/');
+                    $paramArray = explode('/', $paramString);
+                    $paramCount = (empty($paramString)) ? 0 : count($paramArray);
+
+
+                    $reflectionMethod = new \ReflectionMethod($controllerName, $methodName);
+
+                    $optArgCount = 0;
+
+                    foreach ($reflectionMethod->getParameters() as $param) {
+
+                        if ($param->isOptional()) {
+                            $optArgCount++;
                         }
+                    }
+                    // Get All Available params
+                    $numOfArgs = count($reflectionMethod->getParameters()) - $optArgCount;
+
+                    if ($paramCount < $numOfArgs) {
+                        echo 'Wrong Param count!';
+                        die;
+                    }
 
 
-						$controllerName = "Shadowapp\\Controllers\\" . ucfirst( $routedArg['controller'] ) . "Shadow";
+                    $obj = new $controllerName;
 
+                    if (empty($paramCount)) {
+                        $reflectionMethod->invoke($obj);
+                    } else {
+                        call_user_func_array([
+                            $obj, $methodName
+                                ], $paramArray);
+                    }
+                } else {
+                    echo "Can't load  " . $methodName;
+                    die;
+                }
+            } else {
 
-						$methodName = isset( $routedArg['method'] ) ? $routedArg['method'] : null;
+                new $controllerName;
+            }
+            #code here
+        }
+        /*
+         * If Argument Is Function
+         */ else {
+            call_user_func($routedArg);
+        }
+        // Wrong Request method else
+    }
 
+    private static function apiEndpointExists($expectedEndpoint) {
+        return (in_array($expectedEndpoint, self::getListOfApiEndpoints())) ? true : false;
+    }
 
-						/*
-						 * Check If Class Exists
-						 */
-						if ( class_exists( $controllerName ) == false )
-						{
+    private static function getListOfApiEndpoints() {
 
-							echo $controllerName . " doesn't exists";
-							return;
-						}
-
-						/*
-						 * Check if Method Exists
-						 */
-
-						if ( isset( $routedArg['method'] ) )
-						{
-
-							if ( method_exists( $controllerName, $methodName ) )
-							{
-
-								$paramString = trim( substr( $uri, strlen( $routedUri ) ), '/' );
-								$paramArray = explode( '/', $paramString );
-								$paramCount = (empty( $paramString )) ? 0 : count( $paramArray );
-
-
-								$reflectionMethod = new \ReflectionMethod( $controllerName, $methodName );
-							
-								$optArgCount = 0;
-
-								foreach ( $reflectionMethod->getParameters() as $param )
-								{
-
-									if ( $param->isOptional() )
-									{
-										$optArgCount++;
-									}
-								}
-								// Get All Available params
-								$numOfArgs = count( $reflectionMethod->getParameters() ) - $optArgCount;
-
-								if ( $paramCount < $numOfArgs )
-								{
-									echo 'Wrong Param count!';
-									die;
-								}
-
-								
-					 			$obj = new $controllerName; 
-                                
-                                if (empty( $paramCount )) {
-                                  $reflectionMethod->invoke( $obj ); 
-                                } else {
-                                	call_user_func_array( [
-										$obj, $methodName
-												], $paramArray );
-                                }
-							}
-							else
-							{
-								echo "Can't load  " . $methodName;
-								die;
-							}
-						}
-						else
-						{
-
-							new $controllerName;
-						}
-						#code here
-					}
-					/*
-					 * If Argument Is Function
-					 */
-					else
-					{
- 						call_user_func( $routedArg );
-					}
-				 // Wrong Request method else
-	}
-
-	private static function apiEndpointExists( $expectedEndpoint )
-	{
-      return (in_array( $expectedEndpoint, self::getListOfApiEndpoints() )) ? true : false ;
-	}
-
-
-	private static function getListOfApiEndpoints()
-	{
-		
-		$endpoints = [];
-        $t = shcol(self::$_currentRequstMethod,self::$apiRoutes);
+        $endpoints = [];
+        $t = shcol(self::$_currentRequstMethod, self::$apiRoutes);
 
         if (empty($t)) {
             return $endpoints;
         }
-       
-		foreach ($t as $endp => $endpArr ) {
-          $endpoints[] = shcol('apiPrefix',$endpArr)."/".$endp;
-		}
 
-		return $endpoints;
-	}
+        foreach ($t as $endp => $endpArr) {
+            $endpoints[] = shcol('apiPrefix', $endpArr) . "/" . $endp;
+        }
 
-	public static function runAPI()
-	{
-		
-	}
+        return $endpoints;
+    }
 
 }
